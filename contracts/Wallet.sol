08e5e01e2ca9bd02ac769f7d444e78d004b6544f
Ramitphi <18ucs144@lnmiit.ac.in>
2021-06-01 17:05:21 +0530

    withdraw_token_function

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,6 +1,14 @@
-pragma solidity >=0.6.0 < 0.8.0;
+// SPDX-License-Identifier: MIT
 
+pragma solidity >=0.4.22 <0.9.0;
+
+ import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+ import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+   
 contract Wallet{
+    
+    using SafeMath for uint;
+
     struct Token{
         bytes32 ticker;
         address tokenAddress;
@@ -9,5 +17,28 @@ contract Wallet{
     mapping(bytes32 => Token ) public tokenMapping;
     bytes32[] public tokenList;
     mapping(address => mapping(bytes32 =>uint)) public balances;
+
+
+    function addToken(bytes32 ticker,address tokenAddress) public{
+        tokenMapping[ticker]=Token(ticker,tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    function deposit(uint units,bytes32 ticker) external{
+
+        balances[msg.sender][ticker]+=units;
+    }
+
+    
+    function withdraw(uint units,bytes32 ticker) external{
+
+        require(balances[msg.sender][ticker]>=units,"Balance not sufficient");
+        require(tokenMapping[ticker].tokenAddress!=address(0));
+
+        balances[msg.sender][ticker]=balances[msg.sender][ticker].sub(units);
+        IERC20(tokenMapping[ticker].tokenAddress).transfer(msg.sender, units);
+       }
+
+
 
 }
