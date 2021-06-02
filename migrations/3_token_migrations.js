7642a31142c4390a39ad66ab4d072fb98146eeb8
Ramitphi <18ucs144@lnmiit.ac.in>
2021-06-02 19:01:50 +0530

    better_migrations

--- migrations/3_token_migrations.js
+++ migrations/3_token_migrations.js
@@ -1,5 +1,18 @@
-const Migrations = artifacts.require("Link");
+const Link = artifacts.require("Link");
+const Wallet=artifacts.require("Wallet");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer,network,accounts) {
+  await  deployer.deploy(Link);
+  
+  let wallet = await Wallet.deployed();
+  let link = await Link.deployed();
+
+  
+  await link.approve(wallet.address,500);
+  wallet.addToken(web3.utils.fromUtf8("Link"),link.address);
+  await wallet.deposit(100,web3.utils.fromUtf8("Link"));
+
+  let balanceoflink= await wallet.balances(accounts[0],web3.utils.fromUtf8("Link"));
+  console.log(balanceoflink);
+
 };
